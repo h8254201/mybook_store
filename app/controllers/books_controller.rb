429d384b5c51903b7cd15c7748c56480094cba06
h8254201 <h82542011@gmail.com>
2017-05-04 22:50:09 -0400

    finish edit book feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,10 +14,20 @@ class BooksController < ApplicationController
         render :new
       end
   end
-  
-  
-  
-  
+  def edit
+      @book = Book.find_by(id: params[:id])
+  end
+  def update
+      @book = Book.find_by(id: params[:id])
+
+      if @book.update(book_params)
+        # 成功
+        redirect_to books_path, notice: "資料更新成功!"
+      else
+        # 失敗
+        render :edit
+      end
+  end
   private
     def book_params
       params.require(:book).permit(:bookname, :introduction, :authorname, :price)
